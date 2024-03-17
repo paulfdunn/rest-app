7973764c3c6d16ff1acaeb8ae30af8e53a57a0a1
Paul F. Dunn <paulfdunn@gmail.com>
2024-03-17 17:46:21 -0600

    Update docker-run-test.sh

--- example/docker-run-test.sh
+++ example/docker-run-test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# docker-run-test.sh will run the example app in a docker container, get a token
+# then GET the root path, resulting in:
+# `hostname: example, rest-app - from github.com/paulfdunn/rest-app`
 set -x
 
 function cleanup {
@@ -15,20 +18,20 @@ docker network inspect restNet
 if [[ $? != 0 ]]; then
     docker network create restNet -d bridge
 fi
-docker run -d --hostname example --name example --network restNet rest-app/example:v0.0.0
+docker run -p 127.0.0.1:8000:8000/tcp -d --hostname example --name example --network restNet rest-app/example:v0.0.0
 
 # Give time for the container to start.
 sleep 5
-EXAMPLE_IP=$(docker inspect -f '{{.NetworkSettings.Networks.restNet.IPAddress}}' example)
+CONTAINER_IP="127.0.0.1"
 
 echo -e "\n\nget admin token"
 TOKEN_ADMIN=$(curl -k -s -X PUT -d '{"Email":"admin", "Password":"P@ss!234"}' \
-    "https://$EXAMPLE_IP:8000/auth/login/")
+    "https://$CONTAINER_IP:8000/auth/login/")
 echo $TOKEN_ADMIN
 
 HTTP_STATUS=$(curl -k -s -w "\n|HTTP_STATUS=%{http_code}|\n" \
     -H "Authorization: Bearer $TOKEN_ADMIN" \
-    "https://$EXAMPLE_IP:8000/" | \
+    "https://$CONTAINER_IP:8000/" | \
     grep HTTP_STATUS | grep -o -E [0-9]*)
 if [[ $HTTP_STATUS != 200 ]]; then
     echo "user auth was not accepted on root path"
@@ -36,6 +39,6 @@ if [[ $HTTP_STATUS != 200 ]]; then
 fi
 
 echo -e "\n\nGET the root"
-curl -k -s -H "Authorization: Bearer $TOKEN_ADMIN" "https://$PROD1_IP:8000/"
+curl -k -s -H "Authorization: Bearer $TOKEN_ADMIN" "https://$CONTAINER_IP:8000/"
 
 cleanup
