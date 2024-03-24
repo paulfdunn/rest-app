9b2e67cef21d15429fee9c0aa8f38cdf5fdc4d50
Paul F. Dunn <paulfdunn@gmail.com>
2024-03-24 14:55:41 -0600

    Move unsupported files to ./deprecated

--- go-update.sh
+++
@@ -1,5 +0,0 @@
-# When doing updates you can force pull a specific version, without
-# waiting for the indexing, like:
-# `go get -u github.com/paulfdunn/go-helper/logh@v1.0.7`
-find  . -type d -not -path "./.git*" | xargs -I % -L1 sh -c 'cd %; pwd; go get -u; cd ..'
-find  . -type d -not -path "./.git*" | xargs -I % -L1 sh -c 'cd %; pwd; go mod tidy; cd ..'
