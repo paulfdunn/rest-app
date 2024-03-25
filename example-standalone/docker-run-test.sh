8fc75ba0c29948b1d56b5e563966e2837721cb8f
Paul F. Dunn <paulfdunn@gmail.com>
2024-03-25 09:51:28 -0600

    Minor cleanup to docker-run-test.sh

--- example-standalone/docker-run-test.sh
+++ example-standalone/docker-run-test.sh
@@ -1,39 +1,69 @@
 #!/bin/bash
-# docker-run-test.sh will run the example app in a docker container, get a token
+# docker-run-test.sh will run the example-standalone app in a docker container, get a token
 # then GET the root path, resulting in:
-# `hostname: example, rest-app - from github.com/paulfdunn/rest-app`
+# `hostname: example-standalone, rest-app - from github.com/paulfdunn/rest-app`
 set -x
 
-function cleanup {
-docker container stop example
-docker container rm example
+function exitOnError {
+    echo "FAILED: $ME"
+    cleanup
+    exit $HTTP_STATUS
 }
 
+function cleanup {
+docker container stop example-standalone
+docker container rm example-standalone
+}
+
+ME=`basename $0`
+echo "STARTING: $ME"
+
+echo -e "\n\ncleanup prior to start"
 cleanup
 
-docker build -t rest-app/example:v0.0.0 .
+docker build -t rest-app/example-standalone:v0.0.0 .
 
-docker run -p 127.0.0.1:8000:8000/tcp -d --hostname example --name example rest-app/example:v0.0.0
+docker run -p 127.0.0.1:8000:8000/tcp -d --hostname example-standalone --name example-standalone rest-app/example:v0.0.0
 
 # Give time for the container to start.
 sleep 5
-CONTAINER_IP="127.0.0.1"
 
 echo -e "\n\nget admin token"
 TOKEN_ADMIN=$(curl -k -s -X PUT -d '{"Email":"admin", "Password":"P@ss!234"}' \
-    "https://$CONTAINER_IP:8000/auth/login/")
+    "https://127.0.0.1:8000/auth/login/")
 echo $TOKEN_ADMIN
 
 HTTP_STATUS=$(curl -k -s -w "\n|HTTP_STATUS=%{http_code}|\n" \
     -H "Authorization: Bearer $TOKEN_ADMIN" \
-    "https://$CONTAINER_IP:8000/" | \
+    "https://127.0.0.1:8000/" | \
     grep HTTP_STATUS | grep -o -E [0-9]*)
 if [[ $HTTP_STATUS != 200 ]]; then
     echo "user auth was not accepted on root path"
     exitOnError
 fi
 
-echo -e "\n\nGET the root"
-curl -k -s -H "Authorization: Bearer $TOKEN_ADMIN" "https://$CONTAINER_IP:8000/"
+echo -e "\n\n Root path requires auth. Try root path with no auth and get a 401."
+HTTP_STATUS=$(curl -k -s -w "\n|HTTP_STATUS=%{http_code}|\n" \
+     https://127.0.0.1:8000/ | \
+    grep HTTP_STATUS | grep -o -E [0-9]*)
+if [[ $HTTP_STATUS != 401 ]]; then
+    echo "user auth was not required on root path"
+    exitOnError
+fi
 
+echo -e "\n\n Get the root path using the admin token and get a 200."
+HTTP_STATUS=$(curl -k -s -w "\n|HTTP_STATUS=%{http_code}|\n" \
+    -H "Authorization: Bearer $TOKEN_ADMIN" \
+    https://127.0.0.1:8000/ | \
+    grep HTTP_STATUS | grep -o -E [0-9]*)
+if [[ $HTTP_STATUS != 200 ]]; then
+    echo "user auth was not accepted on root path"
+    exitOnError
+fi
+
+
+echo -e "\n\ncleanup and exit"
+ls -al
 cleanup
+
+echo "PASSED: $ME"
